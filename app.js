ae8d122b8e2dfd1d50faf2145d45a26d6f5c2914
Tom Ormiston <tom@appijumbo.com>
2017-08-23 15:47:09 +0100

    created basic node with simple hbs

---
+++ app.js
@@ -0,0 +1,72 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+// Connecting monggose to the router
+mongoose.connect('mongodb://localhost/nodekb');
+
+// to simplify the mongoose.connect syntax
+let db = mongoose.connect;
+
+// Init app
+const app = express();
+
+/*
+hbs.localsAsTemplateData(app);
+app.locals.articles = {
+*/
+
+const port = process.env.PORT || 3000;
+
+//Load view engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// Home route
+app.get('/', (req, res) => {
+
+  let articles = [
+    {
+      'id': 1,
+      'title': 'Aricle One',
+      'author': 'Tom O',
+      'body': 'one one one'
+    },
+    {
+      'id': 2,
+      'title': 'Aricle Two',
+      'author': 'Sarah Jane',
+      'body': 'two two two'
+    },
+    {
+      'id': 3,
+      'title': 'Aricle Three',
+      'author': 'Peter Smith',
+      'body': 'three three three'
+    }
+  ];
+
+   res.render('index.hbs', {
+     pageTitle: 'Articles',
+     message: 'heres the root page',
+     articles: articles
+   });
+});
+
+
+// Article page
+app.get('/article/add', (req, res) => {
+   res.render('article.hbs', {
+     pageTitle: 'Article',
+     message: 'why not add some articles'
+   });
+});
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`Sever on port ${port} is ok`);
+});
---
+++ models/articles.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+//Article Schema
+
+let articleSchema = mongoose.Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  body:{
+    type:String,
+    required: true
+  }
+});
